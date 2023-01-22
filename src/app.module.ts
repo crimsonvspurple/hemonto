1df3aa7d89c102775402b2f28173caf4675cd806
riasat <11375716+crimsonvspurple@users.noreply.github.com>
2023-01-23 02:20:44 +0600

    added sqlite support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: 'db.sqlite',
+      autoLoadEntities: true,
+      synchronize: true, // TODO: remove this in production or use migrations
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
