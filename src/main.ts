5b7cab361d7141ee8d0f2ba6442e42370396d678
riasat <11375716+crimsonvspurple@users.noreply.github.com>
2023-01-23 00:05:50 +0600

    added swagger (<base>/api) so we can easily visualize the APIs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Hemonto API')
+    .setDescription('A Proof of Concept API System')
+    .setVersion('1.0')
+    .addTag('hemonto')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
