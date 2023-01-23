1a992c49905fa64e38d6e94f2ce5279c48cd3646
riasat <11375716+crimsonvspurple@users.noreply.github.com>
2023-01-24 00:47:32 +0600

    fixed test string

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,9 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toBe(
+        'Hello World! Check out the API docs at /api',
+      );
     });
   });
 });
